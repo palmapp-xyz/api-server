0361e4b674936817a9a55b57635b86d004b42f77
naveed-klaytn <naveed.malik@krustuniverse.com>
2023-02-14 01:58:32 +0500

    jwt tests

--- functions/test/jwt.test.ts
+++ functions/test/jwt.test.ts
@@ -44,10 +44,32 @@ describe('JWT API', () => {
     );
 
     // eslint-disable-next-line no-console
-    customToken = response.data.result.token;
+    JWT = response.data.result.idToken;
     console.log(response.data)
     // eslint-disable-next-line no-undef
     expect(response.data.result.idToken.length).to.be.gt(0);
   });
+  it('should create user profile using JWT', async () => {
 
+    // eslint-disable-next-line no-unreachable
+    const response = await Axios.post('https://asia-northeast3-oedi-a1953.cloudfunctions.net/v1/profile/create',
+        {
+          nft_image_url: 'https://example.com/image.png',
+          nft_contract_addr: '0x1234567890abcdef1234567890abcdef12345678',
+          nft_tokenId: '123',
+          bio: 'This is users bio',
+          user_name: 'nickname',
+          sendbird_token: '1234567890abcdef1234567890abcdef12345678'
+        },
+        {
+          headers: {
+            'Authorization': `Bearer ${JWT}`
+          }
+        }
+    );
+    // eslint-disable-next-line no-console
+    console.log(response.data);
+    // eslint-disable-next-line no-undef,no-unused-expressions
+    expect(response.data.message).to.contains('created')
+  });
 });
