07e85b38355b9b43b94eb1e359bc054100bf6262
naveed-klaytn <naveed.malik@krustuniverse.com>
2023-02-22 18:10:48 +0500

    feedRouter

--- functions/src/feed/feedRouter.ts
+++ functions/src/feed/feedRouter.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import {getFeed, getFriendFeed, getFriendsFeed} from './feedController';
-import {isAuthenticated} from "../middlewares/authHandler";
+import {isAuthenticated} from '../middlewares/authHandler';
 // initializing feedRouter
 const feedRouter = express.Router();
 
 // a route to fetch feed of login user
-feedRouter.get('/feed', isAuthenticated, getFeed);
+feedRouter.get('/get', isAuthenticated, getFeed);
 // a route to fetch feed of login user's given friend
-feedRouter.get('/feed/:friendId', isAuthenticated, getFriendFeed);
+feedRouter.get('/get/:friendId', isAuthenticated, getFriendFeed);
 // a route to fetch feed of login user's friends
-feedRouter.get('/friends/feed', isAuthenticated, getFriendsFeed);
+feedRouter.get('/get/friends', isAuthenticated, getFriendsFeed);
 
 export default feedRouter;
 
