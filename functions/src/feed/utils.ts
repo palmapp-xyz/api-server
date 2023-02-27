2ce2dc836354fc648597c8a1023acb67094b34f4
naveed-klaytn <naveed.malik@krustuniverse.com>
2023-02-27 19:11:58 +0500

    error messsages added

--- functions/src/feed/utils.ts
+++ functions/src/feed/utils.ts
@@ -2,9 +2,9 @@
 import {check} from 'express-validator';
 
 // validate pagination parameters
-export const validatePagination = [check('limit').isInt({min: 1, max: 100}), check('offset').isInt({min: 0})];
+export const validatePagination = [check('limit', 'invalid limit').isInt({min: 1, max: 100}), check('offset', 'invalid offset').isInt({min: 0})];
 
 // validate friendId parameter using web3
-export const validateFriendId = [check('friendId').isEthereumAddress()];
+export const validateFriendId = [check('friendId', 'invalid friendId').isEthereumAddress()];
 // validate collection id parameter using web3
-export const validateCollectionAddr = [check('collectionAddr').isEthereumAddress()];
+export const validateCollectionAddr = [check('collectionAddr', 'invalid collectionAddr').isEthereumAddress()];
