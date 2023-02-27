eca8100cf4d5259286fd59456b8ee677616cfc82
naveed-klaytn <naveed.malik@krustuniverse.com>
2023-02-27 19:10:17 +0500

    validators integrated

--- functions/src/feed/feedRouter.ts
+++ functions/src/feed/feedRouter.ts
@@ -1,15 +1,18 @@
 import express from 'express';
-import {getFeed, getFriendFeed, getFriendsFeed} from './feedController';
+import {getFeed, getFriendFeed, getFriendsFeed, getCollectionFeed} from './feedController';
 import {isAuthenticated} from '../middlewares/authHandler';
+import {validateCollectionAddr, validateFriendId, validatePagination} from './utils';
 // initializing feedRouter
 const feedRouter = express.Router();
 
 // a route to fetch feed of login user
-feedRouter.get('/get', isAuthenticated, getFeed);
+feedRouter.get('/get', isAuthenticated, validatePagination, getFeed);
 // a route to fetch feed of login user's given friend
-feedRouter.get('/get/:friendId', isAuthenticated, getFriendFeed);
+feedRouter.get('/get/:friendId', isAuthenticated, validatePagination, validateFriendId, getFriendFeed);
 // a route to fetch feed of login user's friends
-feedRouter.get('/get/friends', isAuthenticated, getFriendsFeed);
+feedRouter.get('/get/friends', isAuthenticated, validatePagination, getFriendsFeed);
+// a route to fetch feed of login user's given collection
+feedRouter.get('/get/collection/:collectionAddr', isAuthenticated, validatePagination, validateCollectionAddr, getCollectionFeed);
 
 export default feedRouter;
 
