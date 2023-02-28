22b3c7cb47d9c71525e40d78ab10990f959feab1
naveed-klaytn <naveed.malik@krustuniverse.com>
2023-02-28 14:24:30 +0500

    changing routes plus making all public

--- functions/src/feed/feedRouter.ts
+++ functions/src/feed/feedRouter.ts
@@ -1,18 +1,17 @@
 import express from 'express';
 import {getFeed, getFriendFeed, getFriendsFeed, getCollectionFeed} from './feedController';
-import {isAuthenticated} from '../middlewares/authHandler';
 import {validateCollectionAddr, validateFriendId, validatePagination} from './utils';
 // initializing feedRouter
 const feedRouter = express.Router();
 
 // a route to fetch feed of login user
-feedRouter.get('/get', isAuthenticated, validatePagination, getFeed);
+feedRouter.get('/', /* isAuthenticated,*/ validatePagination, getFeed);
 // a route to fetch feed of login user's given friend
-feedRouter.get('/get/:friendId', isAuthenticated, validatePagination, validateFriendId, getFriendFeed);
+feedRouter.get('/friends/:friendId', /* isAuthenticated,*/ validatePagination, validateFriendId, getFriendFeed);
 // a route to fetch feed of login user's friends
-feedRouter.get('/get/friends', isAuthenticated, validatePagination, getFriendsFeed);
+feedRouter.get('/friends', /* isAuthenticated,*/ validatePagination, getFriendsFeed);
 // a route to fetch feed of login user's given collection
-feedRouter.get('/get/collection/:collectionAddr', isAuthenticated, validatePagination, validateCollectionAddr, getCollectionFeed);
+feedRouter.get('/collection/:collectionAddr', /* isAuthenticated,*/ validatePagination, validateCollectionAddr, getCollectionFeed);
 
 export default feedRouter;
 
