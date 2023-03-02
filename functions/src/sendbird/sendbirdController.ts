47c03ec48faa2cefee40883a92d3f5438f61e21b
naveed-klaytn <naveed.malik@krustuniverse.com>
2023-03-03 02:04:40 +0500

    sendbird refresh session token

---
+++ functions/src/sendbird/sendbirdController.ts
@@ -0,0 +1,29 @@
+import {Request, Response, NextFunction} from 'express';
+import {firestore as db} from 'firebase-admin';
+import Axios from 'axios';
+
+// initializing sendbird sdk
+
+// get sendbird app id from env
+const appId = process.env.SENDBIRD_APP_ID;
+export const refreshSessionToken = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const URL = `https://api-${appId}.sendbird.com/v3/users/${res.locals.displayName}/token`;
+    const response = await Axios.post(URL, {
+      // set token expiration time 1 day
+      'expires_at': Math.floor(Date.now() / 1000) + 86400,
+    });
+    await db()
+        .collection('profile')
+        .doc(res.locals.displayName)
+        .set({
+          'sendbird_token': response.data.access_token,
+        },
+        {
+          merge: true,
+        });
+    return res.status(200).json({token: response.data.access_token});
+  } catch (error) {
+    next(error);
+  }
+};
