87a534639f4955ab5588a067d43e112ffcfb989d
naveed-klaytn <naveed.malik@krustuniverse.com>
2023-03-02 10:45:55 +0500

    chainId param validator

--- functions/src/feed/utils.ts
+++ functions/src/feed/utils.ts
@@ -2,7 +2,12 @@
 import {check} from 'express-validator';
 
 // validate pagination parameters
-export const validatePagination = [check('limit', 'invalid limit').isInt({min: 1, max: 100}), check('offset', 'invalid offset').isInt({min: 0})];
+export const validatePagination = [
+  check('limit', 'invalid limit').isInt({min: 1, max: 100}),
+  check('offset', 'invalid offset').isInt({min: 0})];
+
+// validate chainId parameter
+export const validateChainId = [check('chainId', 'invalid chainId').isInt({min: 1})];
 
 // validate friendId parameter using web3
 export const validateFriendId = [check('friendId', 'invalid friendId').isEthereumAddress()];
