5b8cc0ca9287c8fbfc21b334c95cf0621a96138f
naveed-klaytn <naveed.malik@klaytn.foundation>
2023-04-06 05:43:10 +0500

    doc + dev -> v1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -46,7 +46,7 @@ app.use('/search', searchRouter);
 app.get('/docs', swaggerui.setup(import('../swagger.json')));
 app.use(errorHandler);
 // functions should be deployed to specific region 'asia-northeast3'
-export const dev = functions.region('asia-northeast3').https.onRequest(app);
+export const v1 = functions.region('asia-northeast3').https.onRequest(app);
 
 
 export const onProfileCreate = functions.firestore
