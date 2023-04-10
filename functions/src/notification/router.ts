68f498498bca34fa7886ba4cd5b9a32c652a7b67
naveed-klaytn <naveed.malik@klaytn.foundation>
2023-04-10 15:55:45 +0500

    /notification/send API

---
+++ functions/src/notification/router.ts
@@ -0,0 +1,23 @@
+// express Router
+import {Router} from 'express';
+import {sendNotification} from './controller';
+import {isAuthenticated} from "../middlewares/authHandler";
+export const notificationRouter = Router();
+/**
+ * @typedef {object} Notification
+ * @property {string} message.required - message to send - eg: 'Hello World!'
+ * @property {string[]} tokens.required - tokens of the users to send notification to - eg '123456789'
+ *
+ */
+
+/**
+ * POST /notification/send
+ * @summary Send notification to user
+ * @tags Notification
+ * @param {Notification} request.body.required - notification to send
+ * @return {object} 200 - An object of notification result
+ * @return {Error}  default - Unexpected error
+ * @security JWT
+ *
+ */
+notificationRouter.post('/send', /** isAuthenticated, */ sendNotification);
